d8f7c1fed37ae5cae91df20133cc5a8f0471aab6
Konstantinos Kavvadias <kavvkon@gmail.com>
2018-01-14 17:26:29 +0100

    First attempt to run unit tests for gdxcc
    Second attempt
    
    Travis osx fix and readme
    
    4
    
    troubleshoot
    
    5
    
    troubleshoot..

---
+++ tests/test_gdx.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+from gdxcc import *
+import sys
+import os
+
+def test_write_gdx():
+
+    GAMS_DIR = os.path.join(os.environ['HOME'], 'gams')
+    print("using GAMS system directory:", GAMS_DIR)
+
+    gdxHandle = new_gdxHandle_tp()
+    rc = gdxCreateD(gdxHandle, GAMS_DIR, GMS_SSSIZE)
+    assert rc[0], rc[1]
+
+    print("Using GDX DLL version: " + gdxGetDLLVersion(gdxHandle)[1])
+
+    assert gdxOpenWrite(gdxHandle, "demanddata.gdx", "xp_example1")[0]
+    assert gdxDataWriteStrStart(gdxHandle, "Demand", "Demand data", 1, GMS_DT_PAR, 0)
+
+    values = doubleArray(GMS_VAL_MAX)
+
+    values[GMS_VAL_LEVEL] = 324.0
+    gdxDataWriteStr(gdxHandle, ["New-York"], values)
+    values[GMS_VAL_LEVEL] = 299.0
+    gdxDataWriteStr(gdxHandle, ["Chicago"], values)
+    values[GMS_VAL_LEVEL] = 274.0
+    gdxDataWriteStr(gdxHandle, ["Topeka"], values)
+
+    assert gdxDataWriteDone(gdxHandle)
+    print("Demand data written by xp_example1")
+
+    assert not gdxClose(gdxHandle)
+    assert gdxFree(gdxHandle)
+
+    print("All done xp_example1")
